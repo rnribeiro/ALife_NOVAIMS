356570985ebeeae4744df357f1b8516a8749bfb3
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2022-12-15 16:42:13 +0000

    -1 and 1

--- ann.py
+++ ann.py
@@ -8,7 +8,7 @@ class NeuralNetwork(object):
         # random.seed(1) 
 
         # Assign random weights to a 5 x 8 matrix,
-        self.synaptic_weights = divide(random.randint(1, 1000, size=(5, 8)), 1000)
+        self.synaptic_weights = divide(random.randint(-1000, 1000, size=(5, 8)), 1000)
 
     # The Sigmoid function
     def __sigmoid(self, x):
---
+++ test.py
@@ -0,0 +1,4 @@
+from joblib.numpy_pickle_utils import xrange
+from numpy import *
+synaptic_weights = divide(random.randint(-1000, 1000, size=(5, 8)), 1000)
+print(synaptic_weights)
