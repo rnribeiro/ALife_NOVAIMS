d4c0ab14c9a7c9e45a495a5ccdc61c20e8762e3e
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2022-12-29 12:41:08 +0000

    Comments

--- neural_network.py
+++ neural_network.py
@@ -3,12 +3,13 @@ from numpy import *
 
 class NeuralNetwork(object):
     def __init__(self):
-        # Assign random weights to a 5 x 8 matrix,
+        # Assign random weights to a 5 x 8 matrix, between -1 and 1
         self.synaptic_weights = divide(random.randint(-1000, 1000, size=(5, 8)), 1000)
 
     # The Sigmoid function
     def __sigmoid(self, x):
         return 1 / (1 + exp(-x))
 
+    # Get output layer
     def learn(self, inputs):
         return self.__sigmoid(dot(inputs, self.synaptic_weights))
